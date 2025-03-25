a67755f545da64aad2340fe951399cc1728eda52
Andtit4 <andtit4@gmail.com>
2025-03-25 12:03:12 +0000

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: "ds-consulting",
+        script: "npm",
+        args: "run dev",
+        env: {
+          NODE_ENV: "development",
+        },
+      },
+    ],
+  };
+  
