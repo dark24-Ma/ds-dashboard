91f1aa65656cee66c80dce10579076608df1986f
Andtit4 <lirsitogo2021@gmail.com>
2025-09-01 16:24:51 +0000

    fixe send newsletter bug

--- src/views/newsletter/SendNewsletter.vue
+++ src/views/newsletter/SendNewsletter.vue
@@ -62,7 +62,7 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import NewsletterService from '@/services/NewsletterService'
+import NewsletterService from '@/services/NewsLetterService'
 import type { Subscriber } from '@/types/NewsLetter'
 // import AdminLayout from '@/components/layout/AdminLayout.vue'
 
